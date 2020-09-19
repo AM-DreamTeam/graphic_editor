9e85104ef35fc28eda9b680c0f0714fd54651b43
Egor <bronnikov.40@mail.ru>
2020-09-19 17:19:38 +0300

    graphic_basics

--- graphic_basics/graphic_basic.py
+++ graphic_basics/graphic_basic.py
@@ -1,9 +1,10 @@
 # Импортированные модули
 from tkinter import *
 
+
 # Глобальные переменные
-activeBrush = False
-activeCreateLine = False
+old_point = None
+used_events = ['<B1-Motion>', '<ButtonPress-1>', '<ButtonRelease-1>']
 
 root = Tk()
 root.title('Graphic Basic')
@@ -11,37 +12,87 @@ root.title('Graphic Basic')
 frame_main = Frame(root)
 
 
+# Базовые функции
+def unbind_all_events(used_events: list) -> None:
+    ''' Очищает все действующие бинды
+
+        Аргументы:
+            used_events: list - список из всех задействованных событий
+
+        Глобальные перменные:
+            root: tkinter.Tk - приложение
+
+        Возвращает:
+            None
+
+        Побочный эффект:
+            Очищает все бинды - used_events
+    '''
+
+    global root
+
+    for _ in used_events:
+        root.unbind(_)
+
+
 # Функции для отрисовки графики
-def draw_points(event: Event, canvas: Canvas, brush_size: int = 5) -> None:
+def draw_points(event: Event, canvas: Canvas, thickness: int = 5) -> None:
     ''' Рисует точку на месте курсора
 
         Аргументы:
-                event: tkinter.Event - событие, по которому считываем положение курсора
-                canvas: tkinter.Canvas - canvas (слой), на котором рисуем точку
-                brush_size: int - жирность точки
+            event: tkinter.Event - событие, по которому считываем положение курсора
+            canvas: tkinter.Canvas - canvas (слой), на котором рисуем точку
+            thickness: int - жирность точки (овала)
 
         Возвращает:
-                None
+            None
 
         Побочный эффект:
-                Отрисовка точки (овала) на canvas (слой)
-
+            Отрисовка точки (овала) на canvas'e (слое)
     '''
 
     x1, y1 = event.x - 1, event.y - 1
     x2, y2 = event.x + 1, event.y + 1
 
-    canvas.create_oval(x1, y1, x2, y2, fill = 'black', width = brush_size)
+    canvas.create_oval(x1, y1, x2, y2, fill = 'black', width = thickness)
 
 
-def draw_line(event: Event, canvas: Canvas) -> None:
-    canvas.create_line(0, 0, event.x, event.y)
+def draw_line(event: Event, canvas: Canvas, thickness: int = 2) -> None:
+    ''' Рисует линию по заданным точкам
+
+        Аргументы:
+            event: tkinter.Event - событие, по которому считывается положение курсора
+            canvas: tkinter.Canvas - canvas (слой), на котором рисуем прямую
+            thickness: int - жирность линии (отрезка)
+
+        Глобальные перменные:
+            old_point: tuple - старые координаты точки, которая является началом линии (отрезка)
+
+        Возвращает:
+            None
+
+        Побочный эффект:
+            Отрисовка линии по заданным точкам на canvas'e
+    '''
+
+    global old_point
+
+    if str(event.type) == 'ButtonPress':
+        old_point = event.x, event.y
+        draw_points(event, canvas, thickness + 1)
+    elif str(event.type) == 'ButtonRelease':
+        x1, y1 = event.x, event.y
+        x2, y2 = old_point
+        canvas.create_line(x1, y1, x2, y2, width = thickness)
+        draw_points(event, canvas, thickness + 1)
+
 
 # Функции (события) для кнопок
-def btn_clear_Event(canvas: Canvas) -> None:
+def btn_clear_Event(used_events: list, canvas: Canvas) -> None:
     ''' Событие для кнопки btnClear
 
         Аргументы:
+            used_events - список из всех задействованных событий
             canvas: tkinter.Canvas - canvas (слой), который очищается
 
         Возвращает:
@@ -49,67 +100,76 @@ def btn_clear_Event(canvas: Canvas) -> None:
 
         Побочный эффект:
             Очистка canvas'a (слоя)
-
     '''
 
+    unbind_all_events(used_events)
     canvas.delete('all')
 
 
-def btn_brush_Event(event: Event, canvas: Canvas, brush_size: int) -> None:
+def btn_brush_Event(used_events: list, canvas: Canvas, thickness: int = 5) -> None:
     ''' Событие для кнопки btnBrush
 
         Аргументы:
-            event: tkinter.Event - событие, по которому будет вызываться отрисовка точек (овалов)
-            canvas: tkinter.Canvas - canvas (слой), на котором будут отрисовываться
-            brush_size: int - жирность точки (овала)
+            used_events - список из всех задействованных событий
+            canvas: tkinter.Canvas - canvas (слой), на котором будет отрисовываться последовательность точек (овалов)
+            thickness: int - жирность точки (овала)
 
         Глобальные переменные:
-            activeBrush: bool - включает или отключает кисть
             root: tkinter.Tk - приложение
 
         Возвращает:
             None
 
         Побочный эффект:
-            activeBrush: True - на событие event биндиться функция draw_points
-            activeBrush: False - анбиндит событие
+            Очищаются все бинды и создаётся новый бинд на <B1-Motion> - отрисовка последовательности точек (овалов)
     '''
 
-    global activeBrush, root
+    global root
 
-    if activeBrush == False:
-        root.bind(event, lambda e, c = canvas, bs = brush_size: draw_points(e, canvas, bs))
-        activeBrush = True
-    else:
-        root.unbind(event)
-        activeBrush = False
+    unbind_all_events(used_events)
+
+    root.bind('<B1-Motion>', lambda event, c = canvas, t = thickness: draw_points(event, canvas, t))
 
 
-def btn_createLine_Event(event: Event, canvas: Canvas) -> None:
+def btn_createLine_Event(used_events: list, canvas: Canvas, thickness: int = 2) -> None:
+    ''' Событие для кнопки btnCreateLine
 
-    global activeCreateLine, root
+        Аргументы:
+            used_events - список из всех задействованных событий
+            canvas: tkinter.Canvas - canvas (слой), на котором будет отрисовываться линия (отрезок)
+            thickness: int - жирность линии
 
-    if activeCreateLine == False:
-        root.bind(event, lambda e = event, c = canvas: draw_line(e, c))
-        activeCreateLine = True
-    else:
-        root.unbind(event)
-        activeCreateLine = False
+        Глобальная перменная:
+            root: tkinter.Tk - приложение
+
+        Возвращает:
+            None
+
+        Побочный эффект:
+            Очищает все бинды и создаёт 2 новых бинла <ButtonPress-1>, <ButtonRelease-1> - отрисовка линии (отрезка)
+    '''
+
+    global root
+
+    unbind_all_events(used_events)
+
+    root.bind('<ButtonPress-1>', lambda event, c = canvas, t = thickness: draw_line(event, c, t))
+    root.bind('<ButtonRelease-1>', lambda event, c = canvas, t = thickness: draw_line(event, c, t))
 
 
-# Работа с графическим интерфейсом и упаковкам
+# Работа с графическим интерфейсом и упаковками
 frame_main.pack()
 
 canvas = Canvas(frame_main, width = 800, height = 600, bg = 'white')
 canvas.pack(side = RIGHT)
 
-btnClear = Button(frame_main, text = '*отчистить*', command = lambda c = canvas:  btn_clear_Event(c))
+btnClear = Button(frame_main, text = '*отчистить*', command = lambda ue = used_events, c = canvas:  btn_clear_Event(ue, c))
 btnClear.pack(side = TOP, pady = 5)
 
-btnBrush = Button(frame_main, text = '*кисть*', command = lambda e = '<B1-Motion>', c = canvas, bs = 2: btn_brush_Event(e, c, bs))
+btnBrush = Button(frame_main, text = '*кисть*', command = lambda ue = used_events, c = canvas, t = 5: btn_brush_Event(ue, c, t))
 btnBrush.pack(side = TOP, pady = 5)
 
-btnCreateLine = Button(frame_main, text = '*линия*', command = lambda e = '<Button-1>', c = canvas: btn_createLine_Event(e, c))
+btnCreateLine = Button(frame_main, text = '*линия*', command = lambda ue = used_events, c = canvas, t = 2: btn_createLine_Event(ue, c, t))
 btnCreateLine.pack(side = TOP, pady = 5)
 
 root.bind('<Control-x>', quit)
