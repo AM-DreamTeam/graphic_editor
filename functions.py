bae84f4d5b922e26ec09662609c1ec2fda96cc86
AkselMath <61156124+AkselMath@users.noreply.github.com>
2020-11-27 21:12:54 +0300

    Update functions.py

--- functions.py
+++ functions.py
@@ -1,7 +1,9 @@
 from pathlib import Path
 from tkinter import *
 from tkinter import filedialog
+from os import path
 
+import os
 import cv2
 import pytesseract
 from PIL import Image
@@ -16,20 +18,33 @@ class Features:
 
         """ распознаёт текст на изображении """
 
-        files = filedialog.askopenfilenames()
-        nameFile = Path(files[0]).name
+        files = filedialog.askopenfilenames()[0]
+        old_file = os.path.join(files[:files.rindex('/')], Path(files).name)
+        new_file = os.path.join(files[:files.rindex('/')], 'rer' + path.splitext(Path(files).name)[1])
+        os.rename(old_file, new_file)
+
+        new_file = new_file.replace('/', '\\')
+
+        nameFile = Path(new_file).name
         pytesseract.pytesseract.tesseract_cmd = r'C:\Program Files\Tesseract-OCR\tesseract.exe'
         img = cv2.imread(nameFile)
         img = cv2.cvtColor(img, cv2.COLOR_BGR2RGB)
         self.label['text'] = pytesseract.image_to_string(img, lang='rus')
 
-    def imageСompression(self, quality=30):
+        old_file = os.path.join(files[:files.rindex('/')], 'rer' + path.splitext(Path(files).name)[1])
+        new_file = os.path.join(files[:files.rindex('/')], Path(files).name)
+        os.rename(old_file, new_file)
+
+    def imageСompression(self):
 
         """ сжатие изображения """
 
-        if self.entry.get() != '':
-            quality = int(self.entry.get())
+        quality = self.entry.get()
+        if quality.isdigit():
+            quality = int(quality)
+        else:
+            quality = 30
 
         files = filedialog.askopenfilenames()
         for image in files:
-            Image.open(image).save('condensed' + image[image.rindex('/') + 1:], optimize=True, quality=quality)
+            Image.open(image).save('condensed ' + Path(image).name, optimize=True, quality=quality)
