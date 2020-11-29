fb4d09cae9e8b0353ae1f622c86f195ce449baf5
AkselMath <61156124+AkselMath@users.noreply.github.com>
2020-11-29 14:02:20 +0300

    Update app.py

--- app.py
+++ app.py
@@ -5,18 +5,18 @@ from compressionRecognition import core
 class App(Tk):
     def __init__(self):
         super().__init__()
-        self.geometry('800x600')
+        self.geometry('800x600') # размеры окна
 
-        self.entry = Entry(self)
+        self.entry = Entry(self) # поле ввода для качества сжимения изображения
         self.entry.grid(row=2, column=0)
 
-        self.textRecognition = Label(self)
+        self.textRecognition = Label(self) # место ввывода для распознования текста
         self.textRecognition.grid(row=0, column=1)
 
-        tr = core.Features(self.textRecognition, self.entry)
+        tr = core.Features(self.textRecognition, self.entry) # объект каласса, где есть необходимые функции
 
-        self.recognition = Button(self, text="распознание текста", command=tr.recognitionText)
-        self.compression = Button(self, text="сжатие изображения", command=tr.imageСompression)
+        self.recognition = Button(self, text="распознание текста", command=tr.recognitionText) # для распознования
+        self.compression = Button(self, text="сжатие изображения", command=tr.imageСompression) # для сжатия
 
         self.recognition.grid(row=0, column=0, sticky=NW)
         self.compression.grid(row=1, column=0, sticky=NW)
