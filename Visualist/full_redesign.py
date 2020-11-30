8bf42df9bd1018a48429aca6e59ac4fef3ed202a
Adnrey <60915706+SaintOops@users.noreply.github.com>
2020-12-01 02:49:00 +0300

    ooops
    Сделал полный редезайн, добавил три пака иконок, добавил стилей виджетам, настроил работу со встроенными темами.

---
+++ Visualist/full_redesign.py
@@ -0,0 +1,210 @@
+from tkinter import *
+from tkinter import Menu
+import tkinter.ttk as ttk
+from ttkthemes import ThemedStyle
+from PIL import Image, ImageTk
+import tkinter.colorchooser
+
+from graphic_lib import _graphic_core as gcore
+
+
+class MenuBar(Menu):
+    def __init__(self, parent):
+        Menu.__init__(self, parent)
+
+        fileMenu = Menu(self, tearoff=False)
+        self.add_cascade(label="Файл", underline=0, menu=fileMenu)
+        fileMenu.add_command(label="Открыть...")
+        fileMenu.add_command(label="Новый файл")
+        fileMenu.add_command(label="Сохранить...")
+        fileMenu.add_command(label="Выйти", underline=1, command=self.quit)
+
+        helpMenu = Menu(self, tearoff=0)
+        self.add_cascade(label="Ссылки", underline=0, menu=helpMenu)
+        helpMenu.add_command(label="Помощь")
+        helpMenu.add_command(label="О программе")
+
+    def quit(self):
+        sys.exit(0)
+
+class App(Tk):
+    def __init__(self):
+        super().__init__()
+
+        ico = Image.open('images/visualist.png')
+        ico.thumbnail((64, 64), Image.ANTIALIAS)
+        photo = ImageTk.PhotoImage(ico)
+        self.wm_iconphoto(False, photo)
+
+        def image_resize(size: tuple, pic: str):
+            return ImageTk.PhotoImage(Image.open(pic).resize(size))
+
+        self.color_code = ((0.0, 0.0, 0.0), '#b6e2f1')
+        def choose_color():
+            self.color_code = tkinter.colorchooser.askcolor(title="Цвета")
+            print(self.color_code[1])
+
+        # параметры иконок
+        self.dir = "blacktheme"
+
+        # параметры стилизации
+        self.stock_bg = self.color_code[1]
+        self.second_bg = "red"
+        self.font = "courier"
+
+        # стили
+        self.styler = ttk.Style()
+        self.styler.configure("TButton", foreground="black", background=self.stock_bg, relief="flat")
+        self.styler.configure("TSeparator", foreground="red", background="red")
+        self.styler.configure('Red.TLabelframe.Label', font=(self.font, 12), foreground="black", background=self.stock_bg)
+        self.styler.configure("Red.TLabelframe", foreground="red", background=self.stock_bg)
+
+        # меню
+        menuBar = MenuBar(self)
+        self.config(menu=menuBar)
+
+
+
+        # главаный контейнер
+        self.frame_main = Frame(self)
+
+        # часть кода Егора для функционала
+
+        self.canvas = gcore.CustomCanvas(self.frame_main,
+                                         width=gcore.DEFAULT_CANVAS_W, height=gcore.DEFAULT_CANVAS_H,
+                                         bg=gcore.DEFAULT_CANVAS_BG,
+                                         )
+        self.frame_main.grid(row=0, column=0)
+        self.canvas.pack(side=BOTTOM)
+
+        events = gcore.Events(self, gcore.DEFAULT_USED_EVENTS, self.canvas)
+        colors = ('red', 'orange', 'yellow', 'green', 'blue', 'indigo', 'violet')
+        events.event_onCanvas()
+
+        # GUI виджеты
+        self.nb = ttk.Notebook(self.frame_main, width=900, height=90)
+        self.toolbar_1 = ttk.Frame(self.nb)
+        self.nb.add(self.toolbar_1, text='Инструменты')
+
+        self.tool_frame = ttk.PanedWindow(self.toolbar_1)
+
+
+        self.im_brush = image_resize((30, 30), f"images/{self.dir}/brush.jpg")
+        self.btn_brush = ttk.Button(self.toolbar_1, style="TButton", image=self.im_brush,
+                                    command=lambda s=gcore.DEFAULT_BRUSH_SIZE, clr=gcore.DEFAULT_FIRST_COLOR:
+                                    events.event_btnBrush(size=s, color=clr, debug_mode=False))
+
+        self.im_fill = image_resize((30, 30), f"images/{self.dir}/fill.jpg")
+        self.btn_fill = ttk.Button(self.toolbar_1, style="TButton", image=self.im_fill,
+                                   command=lambda c=colors:
+                                   events.event_btnFill(color=self.color_code[1]))
+
+        self.im_fasteraser = image_resize((30, 30), f"images/{self.dir}/fasteraser.jpg")
+        self.btn_fasteraser = ttk.Button(self.toolbar_1, style="TButton", image=self.im_fasteraser,
+                                         command=lambda s=gcore.DEFAULT_ERASER_SIZE:
+                                         events.event_btnEraser(size=s))
+
+        self.im_eraser = image_resize((30, 30), f"images/{self.dir}/eraser.jpg")
+        self.btn_eraser = ttk.Button(self.toolbar_1, style="TButton", image=self.im_eraser,
+                                     command=events.event_btnQuickEraser)
+
+        self.sep_1 = ttk.Separator(self.toolbar_1, style="TSeparator", orient=VERTICAL)
+
+        self.im_color = image_resize((70, 70), f"images/{self.dir}/color.jpg")
+        self.btn_colorchoice = ttk.Button(self.toolbar_1, style="TButton", image=self.im_color, command = choose_color)
+
+        self.sep_2 = ttk.Separator(self.toolbar_1, style="TSeparator", orient=VERTICAL)
+
+        self.im_replace = image_resize((30, 30), f"images/{self.dir}/replace.jpg")
+        self.btn_replace = ttk.Button(self.toolbar_1, style="TButton", image=self.im_replace,
+                                      command=lambda ms=gcore.DEFAULT_MOUSE_SPEED:
+                                      events.event_move(mouse_speed=ms))
+
+        self.im_delete = image_resize((30, 30), f"images/{self.dir}/delete.jpg")
+        self.btn_delete = ttk.Button(self.toolbar_1, style="TButton", image=self.im_delete, command=events.event_btnClear)
+
+        self.im_undo = image_resize((30, 30), f"images/{self.dir}/undo.jpg")
+        self.btn_undo = ttk.Button(self.toolbar_1, style="TButton", image=self.im_undo)
+
+        self.im_redo = image_resize((30, 30), f"images/{self.dir}/redo.jpg")
+        self.btn_redo = ttk.Button(self.toolbar_1, style="TButton", image=self.im_redo)
+
+        self.sep_3 = ttk.Separator(self.toolbar_1, style="TSeparator", orient=VERTICAL)
+
+        self.figure_labelframe = ttk.Labelframe(self.toolbar_1, text="Фигуры", style="Red.TLabelframe")
+
+        self.pic_rectangle = image_resize((25, 20), f"images/{self.dir}/rectangle.jpg")
+        self.btn_rectangle = ttk.Button(self.figure_labelframe, image=self.pic_rectangle,
+                                    command=lambda t=gcore.DEFAULT_THICKNESS, outclr=gcore.DEFAULT_FIRST_COLOR,
+                                                   bgclr=gcore.DEFAULT_SECOND_COLOR:
+                                    events.event_btnCreateRectangle(thickness=t, bgcolor=bgclr, outcolor=outclr),
+                                    style="TButton")
+
+        self.pic_line = image_resize((25, 20), f"images/{self.dir}/line.jpg")
+        self.btn_line = ttk.Button(self.figure_labelframe, image=self.pic_line,
+                               command=lambda t=gcore.DEFAULT_THICKNESS, clr=gcore.DEFAULT_FIRST_COLOR:
+                               events.event_btnCreateLine(thickness=t, color=clr),
+                               style="TButton")
+
+        self.pic_ellipsis = image_resize((25, 20), f"images/{self.dir}/ellipsis.jpg")
+        self.btn_ellipsis = ttk.Button(self.figure_labelframe, image=self.pic_ellipsis,
+                                   command=lambda t=gcore.DEFAULT_THICKNESS, outclr=gcore.DEFAULT_FIRST_COLOR,
+                                                  bgclr=gcore.DEFAULT_SECOND_COLOR: events.event_btnCreateOval(
+                                       thickness=t, bgcolor=bgclr, outcolor=outclr),
+                                   style="TButton")
+
+        self.pic_polygon = image_resize((25, 20), f"images/{self.dir}/polygon.jpg")
+        self.btn_polygon = ttk.Button(self.figure_labelframe, image=self.pic_polygon,
+                                  command=lambda t=gcore.DEFAULT_THICKNESS, outclr=gcore.DEFAULT_FIRST_COLOR,
+                                                 bgclr=gcore.DEFAULT_SECOND_COLOR:
+                                  events.event_btnCreatePolygon(thickness=t, bgcolor=bgclr, outcolor=outclr),
+                                  style="TButton")
+
+
+
+
+
+
+
+
+
+        # упаковка виджетов
+        self.nb.pack(side=TOP, anchor=NW)
+
+        self.tool_frame.grid(column=0, row=0, sticky="NSWE")
+        self.btn_brush.grid(column=0, row=0, sticky="NSWE")
+        self.btn_fill.grid(column=1, row=0, sticky="NSWE")
+        self.btn_fasteraser.grid(column=0, row=1, sticky="NSWE")
+        self.btn_eraser.grid(column=1, row=1, sticky="NSWE")
+
+        self.sep_1.grid(row=0, column=2, rowspan=2, sticky="NSWE")
+
+        self.btn_colorchoice.grid(row=0, column=4, rowspan=2, columnspan=2, sticky="NSWE")
+
+        self.sep_2.grid(row=0, column=6, rowspan=2, sticky="NSWE")
+
+        self.btn_replace.grid(row=0, column=7, sticky="NSWE")
+        self.btn_delete.grid(row=0, column=8, sticky="NSWE")
+        self.btn_undo.grid(row=1, column=7, sticky="NSWE")
+        self.btn_redo.grid(row=1, column=8, sticky="NSWE")
+
+        self.sep_3.grid(row=0, column=9, rowspan=2, sticky="NSWE")
+
+        self.figure_labelframe.grid(row=0, column=10, rowspan=3, sticky="NS")
+        self.btn_rectangle.grid(row=0, column=0)
+        self.btn_line.grid(row=0, column=1)
+        self.btn_ellipsis.grid(row=0, column=2)
+        self.btn_polygon.grid(row=1, column=1)
+
+
+
+
+if __name__ == "__main__":
+    app = App()
+
+    style = ThemedStyle(app)
+    style.set_theme("black") #breeze, black, arc, aquativo
+
+    app.title('Visualist')
+    app.geometry('900x900')
+    app.mainloop()
